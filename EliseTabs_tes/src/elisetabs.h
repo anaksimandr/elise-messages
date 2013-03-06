d9d984f67bcb10a9c9a93bc8a0e09a1386becf35
s.andreenko@gmail.com <s.andreenko@gmail.com@276b5169-ddc9-cd37-d031-5e6b7b238eec>
2013-03-06 08:46:29 +0000

    QML tests

---
+++ EliseTabs_tes/src/elisetabs.h
@@ -0,0 +1,49 @@
+#ifndef ELISETABS_ELISETABS_H_
+#define ELISETABS_ELISETABS_H_
+
+#include <QWidget>
+
+class QApplication;
+class QQuickWindow;
+class QQuickView;
+
+class EliseTabs : public QObject
+{
+	Q_OBJECT
+	//Q_PROPERTY(QString name READ name WRITE setName
+	//		   NOTIFY nameChanged)
+	//Q_PROPERTY(QPoint pos READ getPos WRITE setPos NOTIFY posChanged)
+	Q_PROPERTY(Qt::CursorShape cursorShape READ cursorShape WRITE setCursorShape
+			   NOTIFY cursorShapeChanged)
+private:
+	QQuickView*	view;
+	int			currentShape;
+public:
+	EliseTabs();
+	~EliseTabs();
+
+	//QSize	getSize();
+	//QPoint	getPos();
+	//void	setPos(QPoint);
+	//void	setSize(QSize);
+	Qt::CursorShape		cursorShape() const;
+	Q_INVOKABLE void	setCursorShape(Qt::CursorShape cursorShape);
+	Q_INVOKABLE void	moveLeft(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveLeftTop(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveTop(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveTopRight(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveRight(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveRightBottom(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveBottom(QPoint newPos, QPoint oldPos);
+	Q_INVOKABLE void	moveBottomLeft(QPoint newPos, QPoint oldPos);
+
+	Q_INVOKABLE void	addTab();
+	Q_INVOKABLE void	close();
+	//Q_INVOKABLE void	minimize();
+
+signals:
+	//void	posChanged(QPoint);
+	void	cursorShapeChanged();
+};
+
+#endif // ELISETABS_MAINWINDOW_H_
